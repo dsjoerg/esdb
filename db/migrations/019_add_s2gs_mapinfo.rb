4919072d089b3cb7fd64c75e500a9468b03e06a1
David Joerg <dsjoerg@gmail.com>
2014-08-17 15:02:36 -0400

    migrations actually work from a clean DB now

--- db/migrations/019_add_s2gs_mapinfo.rb
+++ db/migrations/019_add_s2gs_mapinfo.rb
@@ -1,13 +1,13 @@
 Sequel.migration do
   change do
     create_table(:esdb_sc2_match_summary_mapfacts) do
-      primary_key :id, :type=>Bignum
+      primary_key :id
 
       String :map_name, :null=>false, :size => 100
       String :map_description, :null=>false, :size => 1000
       String :map_tileset, :null=>false, :size => 100
 
-      index [:map_name, :map_description, :map_tileset], :name=>:allfields
+      index [:map_name, :map_tileset], :name=>:allfields
       index [:map_name], :name=>:map_name
     end
     
